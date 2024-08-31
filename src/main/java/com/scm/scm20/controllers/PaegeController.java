ada0cb637b2fb817cd45f5eb6467260307fc2586
Shweta9975 <shwetamane9696@gmail.com>
2024-08-31 22:15:23 +0530

    updated color theme

--- src/main/java/com/scm/scm20/controllers/PaegeController.java
+++ src/main/java/com/scm/scm20/controllers/PaegeController.java
@@ -2,7 +2,11 @@ package com.scm.scm20.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -42,6 +46,23 @@ public class PaegeController {
         return "services";
     }
       
-
+   // contact page
+   @GetMapping("/contact")
+   public String contact() {
+    
+       return new String("contact");
+   }
+   
+   //login page
+   @GetMapping("/login")
+   public String login() {
+       return new String("login");
+   }
+  
+   //register
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 
 }
